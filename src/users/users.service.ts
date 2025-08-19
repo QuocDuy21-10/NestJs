72e6945a7bf3243b3415f6cb6ec1579ef21964d7
duydq2110 <quocduy37na@gmail.com>
2025-08-19 21:35:37 +0700

    create a user, hash password

---
+++ src/users/users.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import * as bcrypt from 'bcryptjs';
+
+@Injectable()
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+  hashPassword(password: string) {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+    return hash;
+  }
+  async create(email: string, password: string, name: string) {
+    const hashedPassword = this.hashPassword(password);
+    let user = await this.userModel.create({ email, password: hashedPassword, name });
+    return { user };
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
