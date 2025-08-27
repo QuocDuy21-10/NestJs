277cf71def7662a59ff86f76ce8a648d53b02185
duydq2110 <quocduy37na@gmail.com>
2025-08-27 21:35:25 +0700

    chore(config): setup API versioning

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TransformInterceptor } from './core/transform.interceptor';
 
@@ -33,6 +33,12 @@ async function bootstrap() {
     optionsSuccessStatus: 204,
   });
 
+  // config versioning
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: ['1', '2'],
+  });
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
