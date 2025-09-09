45ba918b7c7532b00c5f70956c5edaccd98634a9
duydq2110 <quocduy37na@gmail.com>
2025-09-09 09:27:20 +0700

    feat(user): update user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,27 +1,57 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({ timestamps: true })
 export class User {
-  @Prop({ required: true, unique: true })
+  @Prop()
+  name: string;
+
+  @Prop({ required: true })
   email: string;
 
   @Prop({ required: true })
   password: string;
 
   @Prop()
-  name: string;
+  age: number;
 
   @Prop()
-  phone: string;
-
-  @Prop()
-  age: string;
+  gender: string;
 
   @Prop()
   address: string;
+
+  @Prop({ type: Object })
+  company: {
+    _id: mongoose.Schema.Types.ObjectId;
+    name: string;
+  };
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  refreshToken: string;
+
+  @Prop({ type: Object })
+  createdBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  updatedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
+
+  @Prop({ type: Object })
+  deletedBy: {
+    _id: mongoose.Schema.Types.ObjectId;
+    email: string;
+  };
 
   @Prop()
   createdAt: Date;
@@ -30,10 +60,10 @@ export class User {
   updatedAt: Date;
 
   @Prop()
-  deletedAt: Date;
+  isDeleted: boolean;
 
   @Prop()
-  isDeleted: boolean;
+  deletedAt: Date;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
