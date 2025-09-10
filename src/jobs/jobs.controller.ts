0a76033dff2491fa3fbfa84ae238ffd89855b9b7
duydq2110 <quocduy37na@gmail.com>
2025-09-10 08:45:30 +0700

    feat(job): create job schema and CRUD APIs

---
+++ src/jobs/jobs.controller.ts
@@ -0,0 +1,46 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { CreateJobDto } from './dto/create-job.dto';
+import { UpdateJobDto } from './dto/update-job.dto';
+import { Public, ResponseMessage, User } from 'src/decorator/customize';
+import { IUser } from 'src/users/users.interface';
+
+@Controller('jobs')
+export class JobsController {
+  constructor(private readonly jobsService: JobsService) {}
+
+  @Post()
+  @ResponseMessage('Job has been created successfully')
+  create(@Body() createJobDto: CreateJobDto, @User() user: IUser) {
+    return this.jobsService.create(createJobDto, user);
+  }
+
+  @Get()
+  @ResponseMessage('Jobs have been retrieved successfully')
+  findAll(
+    @Query('current') currentPage: string,
+    @Query('pageSize') limit: string,
+    @Query() query: string,
+  ) {
+    return this.jobsService.findAll(+currentPage, +limit, query);
+  }
+
+  @Public()
+  @Get(':id')
+  @ResponseMessage('Job has been found successfully')
+  findOne(@Param('id') id: string) {
+    return this.jobsService.findOne(id);
+  }
+
+  @Patch(':id')
+  @ResponseMessage('Job has been updated successfully')
+  update(@Param('id') id: string, @Body() updateJobDto: UpdateJobDto, @User() user: IUser) {
+    return this.jobsService.update(id, updateJobDto, user);
+  }
+
+  @Delete(':id')
+  @ResponseMessage('Job has been deleted successfully')
+  remove(@Param('id') id: string, @User() user: IUser) {
+    return this.jobsService.remove(id, user);
+  }
+}
