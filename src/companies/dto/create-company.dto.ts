d31f61229ed595f400512bd40c7d2dc2697dcb77
duydq2110 <quocduy37na@gmail.com>
2025-09-10 15:33:09 +0700

    feat(companies): update companies schema

--- src/companies/dto/create-company.dto.ts
+++ src/companies/dto/create-company.dto.ts
@@ -1,12 +1,22 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsString, IsUrl, MaxLength } from 'class-validator';
 
 export class CreateCompanyDto {
   @IsNotEmpty({ message: 'Name is required' })
+  @IsString({ message: 'Name must be a string' })
+  @MaxLength(100, { message: 'Name is too long (max: 100 chars)' })
   name: string;
 
   @IsNotEmpty({ message: 'Address is required' })
+  @IsString({ message: 'Address must be a string' })
+  @MaxLength(200, { message: 'Address is too long (max: 200 chars)' })
   address: string;
 
   @IsNotEmpty({ message: 'Description is required' })
+  @IsString({ message: 'Description must be a string' })
+  @MaxLength(2000, { message: 'Description is too long (max: 2000 chars)' })
   description: String;
+
+  @IsNotEmpty({ message: 'Logo is required' })
+  @IsUrl({}, { message: 'Logo must be a valid URL' })
+  logo: String;
 }
