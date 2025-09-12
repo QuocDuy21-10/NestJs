ae7b0ffb529b84b5c6b4dbb6c01f691de7277a50
duydq2110 <quocduy37na@gmail.com>
2025-09-12 20:27:27 +0700

    feat(auth): implement permissions-based authorization

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,6 +1,12 @@
-import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 import { IS_PUBLIC_KEY } from 'src/decorator/customize';
 
 @Injectable()
@@ -19,10 +25,25 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     return super.canActivate(context);
   }
 
-  handleRequest(err, user, info) {
+  handleRequest(err, user, info, context: ExecutionContext) {
+    const request: Request = context.switchToHttp().getRequest();
     // You can throw an exception based on either "info" or "err" arguments
     if (err || !user) {
       throw err || new UnauthorizedException('Token is invalid or expired');
+    }
+
+    // check permissions
+    const targetMethod = request.method;
+    const targetPath = request.route?.path;
+
+    const permissions = user?.permissions ?? [];
+
+    const isExistPermission = permissions.find(permission => {
+      return permission.apiPath === targetPath && permission.method === targetMethod;
+    });
+
+    if (!isExistPermission) {
+      throw new ForbiddenException('You do not have permission to access this resource');
     }
     return user;
   }
